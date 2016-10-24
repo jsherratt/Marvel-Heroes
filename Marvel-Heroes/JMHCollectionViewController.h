333d7fd38db0a0fd2c8b880df40fdb3b95d73a68
Joey <joey@Joeys-iMac.local>
2016-10-24 23:49:13 +0100

    Initial commit

---
+++ Marvel-Heroes/JMHCollectionViewController.h
@@ -0,0 +1,15 @@
+//
+//  JMHCollectionViewController.h
+//  Marvel-Heroes
+//
+//  Created by Joey on 24/10/2016.
+//  Copyright © 2016 Joey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMHCollectionViewController : UICollectionViewController
+
+@property (nonatomic) NSMutableArray *heroArray;
+
+@end
---
+++ Marvel-Heroes/JMHCollectionViewController.m
@@ -0,0 +1,68 @@
+//
+//  JMHCollectionViewController.m
+//  Marvel-Heroes
+//
+//  Created by Joey on 24/10/2016.
+//  Copyright © 2016 Joey. All rights reserved.
+//
+
+#import "JMHCollectionViewController.h"
+
+@interface JMHCollectionViewController ()
+
+@end
+
+@implementation JMHCollectionViewController
+
+static NSString * const reuseIdentifier = @"MarvelHeroCell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Register cell classes
+   // [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.topItem.title = @"Marvel Heroes";
+    
+}
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell
+    cell.backgroundColor = [UIColor blackColor];
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+}
+
+
+ #pragma mark - Navigation
+ 
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+
+ }
+
+
+
+
+@end
